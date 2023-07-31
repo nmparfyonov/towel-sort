88e67b816e70d8b665d2929a8784e46c1b0f89fd
nparfyonov <nikita.parfyonov@hf-solution.com>
2023-07-31 23:05:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+  return Array.isArray(matrix) ? matrix.reduce((a, x, i) => i % 2 ? a.concat(x.reverse()) : a.concat(x), []) : []
+};
